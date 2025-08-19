0458107e2772baff071283ad94dff3ebeb7d02c3
Sutanu1234 <sutanubera82@gmail.com>
2025-08-19 11:03:00 +0530

    Initial commit with project files

---
+++ cloudshareapi/src/main/java/in/bushansirgur/cloudshareapi/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package in.bushansirgur.cloudshareapi.controller;
+
+import in.bushansirgur.cloudshareapi.dto.PaymentDTO;
+import in.bushansirgur.cloudshareapi.dto.PaymentVerificationDTO;
+import in.bushansirgur.cloudshareapi.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/payments")
+@CrossOrigin(origins = "http://localhost:5173")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/create-order")
+    public ResponseEntity<?> createOrder(@RequestBody PaymentDTO paymentDTO) {
+        PaymentDTO response = paymentService.createOrder(paymentDTO);
+
+        if (response.getSuccess()) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @PostMapping("/verify-payment")
+    public ResponseEntity<?> verifyPayment(@RequestBody PaymentVerificationDTO request) {
+       PaymentDTO response = paymentService.verifyPayment(request);
+        if (response.getSuccess()) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+}
